5059c567400177df1eeeed8323c25bd2e5193c40
Stiliyan Kushev <stiliyankushev@gmail.com>
2018-11-28 21:54:46 +0200

    fixed a problem with the redirect ...again

--- javascript/login.js
+++ javascript/login.js
@@ -2,11 +2,15 @@ function login() {
     let beg = window.location.href;
     let url = "";
     let slash_count = 0;
+    let global = false;
     for(let i = 0; i < beg.length;i++){
         if(beg[i] == "/") slash_count++;
         url += beg[i];
-        if(beg[i + 1] == "i" && slash_count == 3) continue;
-        if(slash_count == 3) break;
+        if(beg[i + 1] == "i") {global = true;}
+        if(slash_count == 3 && global == false) break;
+        if(slash_count == 4){
+            break;
+        }
     }
     let username = $("#username").val();
     let password = $("#password").val();
