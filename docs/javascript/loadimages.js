672e084f2b7123142846fbbc208cc1f611aad981
Stiliyan Kushev <stiliyankushev@gmail.com>
2018-11-29 23:03:08 +0200

    now the site can visualize the given from the db data acording to the page you are in (gallery or your_images)

--- docs/javascript/loadimages.js
+++ docs/javascript/loadimages.js
@@ -1,13 +1,13 @@
 let url = window.location.href;
 new Promise(function (resolve, reject) {
-    if(url.includes("your_images")){
+    if (url.includes("your_images")) {
         Req.getAll('images', `?query={"creator":"${Auth.username()}"}&sort={"_kmd.ect": -1}`).then(res => {
             resolve(res);
         }).catch(err => {
             reject(err);
         });
     }
-    else if(url.includes("gallery")){
+    else if (url.includes("gallery")) {
         Req.getAll('images', '?query={}&sort={"_kmd.ect": -1}').then(res => {
             resolve(res);
         }).catch(err => {
@@ -15,8 +15,39 @@ new Promise(function (resolve, reject) {
         });
     }
 }).then(res => {
-    for(let image of res){
-        $(".grid").append($(`<div class="item""></div>`).css("background",`url(${image.link})`));
+    for (let image of res) {
+        let edit_btn = $(`<input type="button" value="EDIT">`);
+        let delate_btn = $(`<input type="button" value="DELATE">`);
+
+        var img = new Image();
+        img.onload = function () {
+            let item;
+
+            if (url.includes("your_images")) {
+                item = $(`
+                <div class="item"" item_id="${image._id}"></div>
+                `).css("background", `url(${image.link})`).append($(`
+                <div>
+                </div>`).append(edit_btn).append(delate_btn));
+            }
+            else if (url.includes("gallery")) {
+                item = $(`
+                <div class="item"" item_id="${image._id}"></div>
+                `).css("background", `url(${image.link})`).append($(`
+                <div>
+                </div>`));
+            }
+
+            if (this.width > this.height + 50) {
+                item.addClass("landscape");
+            }
+            else if (this.height > this.width + 50) {
+                item.addClass("portrait");
+            }
+
+            $(".grid").append(item);
+        }
+        img.src = `${image.link}`;
     }
 
     console.log(res);
