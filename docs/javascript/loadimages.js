a8ac20c3417657743ad1dbab3a80ed04b24ed885
Stiliyan Kushev <stiliyankushev@gmail.com>
2018-11-29 20:33:44 +0200

    added a grid functionality for the your images

--- docs/javascript/loadimages.js
+++ docs/javascript/loadimages.js
@@ -1,15 +1,25 @@
 let url = window.location.href;
-if(url.includes("your_images")){
-    Req.getAll('images', `?query={"creator":"${Auth.username()}"}&sort={"_kmd.ect": -1}`).then(res => {
-        console.log(res);
-    }).catch(err => {
-        console.log(err);
-    });
-}
-else if(url.includes("gallery")){
-    Req.getAll('images', '?query={}&sort={"_kmd.ect": -1}').then(res => {
-        console.log(res);
-    }).catch(err => {
-        console.log(err);
-    });
-}
+new Promise(function (resolve, reject) {
+    if(url.includes("your_images")){
+        Req.getAll('images', `?query={"creator":"${Auth.username()}"}&sort={"_kmd.ect": -1}`).then(res => {
+            resolve(res);
+        }).catch(err => {
+            reject(err);
+        });
+    }
+    else if(url.includes("gallery")){
+        Req.getAll('images', '?query={}&sort={"_kmd.ect": -1}').then(res => {
+            resolve(res);
+        }).catch(err => {
+            reject(err);
+        });
+    }
+}).then(res => {
+    for(let image of res){
+        $(".grid").append($(`<div class="item""></div>`).css("background",`url(${image.link})`));
+    }
+
+    console.log(res);
+}).catch(err => {
+    console.log(err);
+});
